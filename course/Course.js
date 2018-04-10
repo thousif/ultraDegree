88ec0c509b1ffd55497143075ab149420ec14ca6
Thousif jowhar <thousif20@gmail.com>
2018-04-10 08:50:17 +0530

    adding updates

--- course/Course.js
+++ course/Course.js
@@ -9,20 +9,18 @@ var course = new mongoose.Schema(
 		u_tag          : { type : String, required : true },        // unique tag
 		desc 	         : { type : String, required : true },        // description of the course
 		t_url       	 : { type : String, required : true },				// thuimb url
-		tag 	         : { type : String, required : true },        // tagline of the course
 		i_paid         : { type : Boolean, default : false },       // is the course paid
 		type           : { type : Number, default : 1 },            // type of the course
-		catg           : { type : Number, default : 1 },						// category of the course
 		duration       : String,																		// course duration string
 		status         : { type : Number, default : 1 },            // status of the course
 		i_seats_l      : { type : Boolean, default : false },       // is seats limited for the course
 		seats_n        : Number,																	  // number of seats if limited above is true
-		// l_date         : Date,                                      // launch date of the course
 		aid 	         : { type : ObjectId, required : true },      // account id of the course creater	
-	},
-	{ 
+	},{
 		collection : 'course'
 	}
 );
 
 mongoose.model('course',course);
+
+module.exports = mongoose.model('course');
--- course/CourseController.js
+++ course/CourseController.js
@@ -11,12 +11,11 @@ var Course = require('./Course');
 // Added VerifyToken middleware to make sure only an authenticated user can put to this route
 router.get('/list', /* VerifyToken, */ function (req, res) {
     console.log('fetching all courses from db');
-    Course.find(function (err, course) {
+    Course.find({},function (err, course) {
         console.log(err,course);
         if (err) return res.status(500).send("There was a problem fetching the courses.");
-        res.status(200).send(courses);
+        res.status(200).send(course);
     });
 });
-
 
 module.exports = router;
